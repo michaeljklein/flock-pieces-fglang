09cb0c08e0c9f3cc889a6905b9faebf958d9aeae
michaeljklein <michael14486@gmail.com>
2018-01-18 14:55:28 -0500

    got demo working

--- README.md
+++ README.md
@@ -3,6 +3,19 @@
 A very small term-rewriting language, related to the study of push-throughs.
 
 
+## Demo executable
+
+```bash
+> ./.stack-work/install/x86_64-osx/lts-10.2/8.2.2/bin/fglang-demo 4
+["GFG","GFF","GF"]
+["GGFF","GGFG","FGFG","FGFF"]
+["FFGFG","GFGFF","FFGFF","GFGFG","GGGFG","GGGFF","FGGFF","FGGFG"]
+["FGGGFG","FGGGFF","GFGGFG","FFGFF","GFGGFF","GFGFG","FFGGFF","FFGGFG","FFGF","GGGGFF","GGGGFG","GGGF","GGGFG","FGGF","FGFGFF","GFGF","FFFGFG","FGFGFG","FFFGFF","GGFGFG","FGGFF","GFFGFF","GGFGFF","GFFGFG"]
+```
+
+
+## Tree
+
 ```bash
 > git rev-parse HEAD
 73d42c0cdc7f9747c32cdc3c5051e7f1398e0426
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,17 @@
 module Main where
 
+import System.Environment (getArgs)
+import Data.FGLang (Expr(..), rs, bases)
+import Data.Char (isDigit)
+import Data.Foldable (toList)
+import qualified Data.HashSet as S (toList)
 
 main :: IO ()
-main = return ()
-
+main = do
+  args <- getArgs
+  if (length args /= 1) || (not . null . filter (not . isDigit) . head) args
+     then putStrLn "Please provide a single argument: how many iterations of Data.FGLang.rs to print"
+     else do
+       let n = read (head args)
+       mapM_ print . map (map (map (\x -> if x then 'G' else 'F') . toList . getExpr) . S.toList) . take n . iterate rs $ bases
 
