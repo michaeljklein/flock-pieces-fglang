3d7a258dd3f97b9f6ea6c9a993c9d220de533267
Michael J. Klein <lambdamichael@gmail.com>
2018-10-08 21:26:49 -0400

    remove missing test

--- test/Test.hs
+++ test/Test.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import qualified Data.Flip (quickTestBatches)
 import Data.Hashable.Orphans (quickCheckHashableSeq)
 import Data.FGLang (quickCheckFGLang)
 import Test.QuickCheck (quickCheck)
@@ -8,9 +7,6 @@ import Data.HashSet.Utils (quickCheckHashSetUtils)
 
 main :: IO ()
 main = do
-  putStrLn "Data.Flip:"
-  Data.Flip.quickTestBatches
-
   putStrLn "Data.Hashable.Orphans:"
   print =<< quickCheckHashableSeq
 
